3497451be3a506f280e9d269b57244e99076e8de
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-06 14:08:03 -0500

    Day01: FindElementById for search box

--- src/test/java/com/cybertek/day01_navigation_locators/FindElementById.java
+++ src/test/java/com/cybertek/day01_navigation_locators/FindElementById.java
@@ -1,15 +1,28 @@
 package com.cybertek.day01_navigation_locators;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementById {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        // Open chrome and navigate to yahoo
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.yahoo.com/");
+
+        // Locate search box and enter selenium
+        WebElement searchBox = driver.findElement(By.id("ybar-sbq"));
+
+        // Enter text inside inputbox element using sendKey("your text")
+        searchBox.sendKeys("Selenium");
+
+        Thread.sleep(5000);
+        // Quit browser
+        driver.quit();
     }
 }
