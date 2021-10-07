835dbcf326792e328f71092be806dd100aee8a1b
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-07 09:20:21 -0500

    Day02: LinkText Practice completed

--- src/test/java/com/cybertek/day02_locators_practice/LinkTextPartialLinkTextPractice.java
+++ src/test/java/com/cybertek/day02_locators_practice/LinkTextPartialLinkTextPractice.java
@@ -1,7 +1,9 @@
 package com.cybertek.day02_locators_practice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LinkTextPartialLinkTextPractice {
@@ -11,5 +13,30 @@ public class LinkTextPartialLinkTextPractice {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cybertekschool.com/");
+
+        // Click on first link "A/B Testing"
+        // WebElement firstLnk = driver.findElement(By.linkText("A/B Testing"));
+        // firstLnk.click();
+        driver.findElement(By.linkText("A/B Testing")).click();
+
+        // Navigate back to home page
+        driver.navigate().back();
+
+        // Maximize the screen here
+        driver.manage().window().maximize();
+
+        // Click on second link, either full linkText or partialLinkText
+        // driver.findElement(By.linkText("Add/Remove Elements")).click();
+        WebElement addOrRemoveLink = driver.findElement(By.partialLinkText("Add/Remove"));
+        addOrRemoveLink.click();
+
+        // Click on "Home" link to go back to home
+        driver.findElement(By.linkText("Home")).click();
+
+        // Click on "Redirect Link"
+        driver.findElement(By.linkText("Redirect Link")).click();
+
+        // Close browser
+        driver.quit();
     }
 }
