a35ac534f4293632e365ade9b2f16b56a74f4108
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-07 11:20:34 -0500

    Day03: Getting attribute of elements practice completed

--- src/test/java/com/cybertek/day03_locate_by_tag_class_getAtt/GetElementAttribute.java
+++ src/test/java/com/cybertek/day03_locate_by_tag_class_getAtt/GetElementAttribute.java
@@ -24,6 +24,29 @@ public class GetElementAttribute {
         // Get any attribute of identified element, in this case href
         System.out.println("firstLink.getAttribute(\"href\") = " + firstLink.getAttribute("href"));
 
+        // Navigate to google.com, identify the search box and get couple attributes value
+        driver.get("https://www.google.com/");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium");
+
+        // Get the class attribute
+        System.out.println("searchBox.getAttribute(\"class\") = " + searchBox.getAttribute("class"));
+
+        // maxlength
+        System.out.println("searchBox.getAttribute(\"maxlength\") = " + searchBox.getAttribute("maxlength"));
+
+        // title
+        System.out.println("searchBox.getAttribute(\"title\") = " + searchBox.getAttribute("title"));
+
+        // autocapitalize
+        System.out.println("searchBox.getAttribute(\"autocapitalize\") = " + searchBox.getAttribute("autocapitalize"));
+
+        // spellcheck
+        System.out.println("searchBox.getAttribute(\"spellcheck\") = " + searchBox.getAttribute("spellcheck"));
+
+        // role
+        System.out.println("searchBox.getAttribute(\"role\") = " + searchBox.getAttribute("role"));
+
         driver.quit();
     }
 }
