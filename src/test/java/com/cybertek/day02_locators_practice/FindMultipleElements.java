5445d27f978cadce85c7f28d639f92aafc338465
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-07 10:14:16 -0500

    Day02: findElements and get the text of each element in loop

---
+++ src/test/java/com/cybertek/day02_locators_practice/FindMultipleElements.java
@@ -0,0 +1,42 @@
+package com.cybertek.day02_locators_practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class FindMultipleElements {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/");
+
+        // Get all the links that have partial text A in it
+        List<WebElement> allLinks = driver.findElements(By.partialLinkText("A"));
+        // Get the size of this list so we know how many link with partial text A
+        System.out.println("allLink.size() = " + allLinks.size());
+
+        // Get the first item and get text
+        WebElement firstItem = allLinks.get(0);
+        System.out.println("firstItem.getText() = " + firstItem.getText());
+
+        WebElement secondItem = allLinks.get(1);
+        System.out.println("secondItem.getText() = " + secondItem.getText());
+
+        // Iterate over whole list and print out the text
+        for (WebElement eachElement : allLinks) {
+            System.out.println("eachElement.getText() = " + eachElement.getText());
+        }
+
+        // findElement method will throw exception when it cannot find element
+        // findElements method will return empty List<WebElements>
+
+        // Close the browser
+        driver.quit();
+    }
+}
