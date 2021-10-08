a50df327115b3d6a69f7df9a8a34fcfc55e176f8
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-08 01:14:53 -0500

    Day04: Working with radio buttons completed

--- src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/LocatingRadioButtons.java
+++ src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/LocatingRadioButtons.java
@@ -1,8 +1,12 @@
 package com.cybertek.day04_common_elements_xpath_css_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class LocatingRadioButtons {
 
@@ -15,6 +19,49 @@ public class LocatingRadioButtons {
         // Open chrome navigate here http://practice.cybertekschool.com/radio_buttons
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("http://practice.cybertekschool.com/checkboxes");
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+
+        // Locate the blue radio button
+        WebElement blueRadio = driver.findElement(By.id("blue"));
+        System.out.println("blueRadio.isSelected() = " + blueRadio.isSelected()); // true because it's already selected
+        blueRadio.click(); // Nothing will happen because it is already selected
+
+        WebElement redRadio = driver.findElement(By.id("red"));
+        System.out.println("before click redRadio.isSelected() = " + redRadio.isSelected());
+        redRadio.click(); // This will click to check radio button
+        System.out.println("after click redRadio.isSelected() = " + redRadio.isSelected());
+
+        // Check blue radio button status
+        System.out.println("after red is clicked blueRadio.isSelected() = " + blueRadio.isSelected());
+
+        WebElement greenRadio = driver.findElement(By.id("green"));
+        greenRadio.click(); // Nothing will happen
+        System.out.println("greenRadio.isSelected() = " + greenRadio.isSelected());
+        // Any element can use isEnabled() method to check whether element is disabled or not
+        // One can not take any action on disabled element
+        System.out.println("greenRadio.isEnabled() = " + greenRadio.isEnabled());
+
+        // A radio group is grouped by its name attribute
+        // All radio buttons related to color has name color in this case
+        // So we can findElements to get all of them
+        List<WebElement> allColorRadios = driver.findElements(By.name("color"));
+
+        // Check the size to see how many radio button identified
+        System.out.println("allColorRadios.size() = " + allColorRadios.size());
+
+        // Click on 3rd radio button
+        allColorRadios.get(2).click();
+
+        // Loop over all the elements to print these: id attribute, isSelected, isEnabled
+        // allColorRadios.iter and hit enter, then give each element a name
+        for (WebElement eachColorRadio : allColorRadios) {
+            System.out.println("eachColorRadio.getAttribute(\"id\") = " + eachColorRadio.getAttribute("id"));
+            System.out.println("eachColorRadio.isSelected() = " + eachColorRadio.isSelected());
+            System.out.println("eachColorRadio.isEnabled() = " + eachColorRadio.isEnabled());
+            System.out.println("---------------------------------------------");
+        }
+
+        // Close browser
+        driver.quit();
     }
 }
