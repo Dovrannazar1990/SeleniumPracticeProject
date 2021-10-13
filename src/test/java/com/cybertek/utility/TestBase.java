918f0688d1c91e961fe04ec8a6a6af9a3b4e42da
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-13 17:01:14 -0500

    Day06: TestBase class to remove duplicate

---
+++ src/test/java/com/cybertek/utility/TestBase.java
@@ -0,0 +1,28 @@
+package com.cybertek.utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * This class is meant to be super class to provide
+ * driver set up and closing browser for it's subclasses
+ */
+public abstract class TestBase {
+
+    WebDriver driver;
+    // Setting up all driver stuff here directly in @BeforeEach method
+    @BeforeEach
+    public void setUpWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
