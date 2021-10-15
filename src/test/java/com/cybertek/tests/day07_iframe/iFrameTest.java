a9d53707b1758301e6b40d767e5d2a653c78de38
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-15 11:18:17 -0500

    Day07: iFrame Code Demo completed

--- src/test/java/com/cybertek/tests/day07_iframe/iFrameTest.java
+++ src/test/java/com/cybertek/tests/day07_iframe/iFrameTest.java
@@ -12,11 +12,35 @@ public class iFrameTest extends TestBase {
 
         driver.get("http://practice.cybertekschool.com/iframe");
 
-        // Locate the <p> element inside iframe
-        // First we need to switch to the iFrame: by index
-        driver.switchTo().frame(0);
+        /*
+         * Locate the <p> element inside iframe
+         * First we need to switch to the iFrame:
+         *      by index (index in java starts with 0, we are calling java method here)
+                driver.switchTo().frame(0);
+         *
+         *      by name or id
+                driver.switchTo().frame("mce_0_ifr");
+         *
+         *      by passing iframe WebElement object
+         *      locate the iframe element by it's title "Rich Text Area. Press ALT-0 for help."
+         * Css selector locator: iframe[title='Rich Text Area. Press ALT-0 for help.']
+         *                       iframe[title^='Rich Text Area']
+       */
+        WebElement iframeElm = driver.findElement(By.cssSelector("iframe[title^='Rich Text Area']"));
+        driver.switchTo().frame(iframeElm);
+
         WebElement pTagInsideFrame = driver.findElement(By.tagName("p"));
         System.out.println("pTagInsideFrame.getText() = " + pTagInsideFrame.getText());
 
+        /*
+         * 2 ways to get out of the frame
+         * Jump one level out of the frame
+                driver.switchTo().parentFrame();
+         * Jump all the way to the main content, this will get you out of any level deep inside the frame
+         */
+        driver.switchTo().defaultContent();
+
+        // Click on the Home link on top left corner
+        driver.findElement(By.linkText("Home")).click();
     }
 }
