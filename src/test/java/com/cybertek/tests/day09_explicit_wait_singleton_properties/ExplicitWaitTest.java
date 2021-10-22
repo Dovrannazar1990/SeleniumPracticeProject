009b429e023e8a191fd0561e37bec19498274446
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-21 19:37:04 -0500

    Day09: Explicit wait practice to print before and after text warning area

--- src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest.java
+++ src/test/java/com/cybertek/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest.java
@@ -3,6 +3,8 @@ package com.cybertek.tests.day09_explicit_wait_singleton_properties;
 import com.cybertek.utility.TestBase;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
@@ -25,7 +27,7 @@ public class ExplicitWaitTest extends TestBase {
         // ExpectedConditions helper class has a lot of pre-written conditions
         // For common scenarios, so we don't have to build our own
         // In this case we are checking for title equal Dynamic title
-        // wait.until(ExpectedConditions.titleIs("Dynamic title"));
+        wait.until(ExpectedConditions.titleIs("Dynamic title"));
 
         // Try different condition as below
         // Wait until the spongebob image visible on screen
@@ -36,8 +38,31 @@ public class ExplicitWaitTest extends TestBase {
 
         // We did static import as below so pure code can be shorter
         // import static org.openqa.selenium.support.ui.ExpectedConditions.*;
-        wait.until(visibilityOfElementLocated(By.xpath("//img[@alt='square pants']")));
+        // wait.until(visibilityOfElementLocated(By.xpath("//img[@alt='square pants']")));
 
+        // The warning area initially has text of this page is slow wait for 5 seconds...
+        // After it loading the text of that area become Done!
+        // This is one of the ExpectedConditions we can use for waiting
         System.out.println("The end");
+    }
+
+    @Test
+    public void testByTextToBe() {
+        // 1. NalertArea = driver.findElement(By.id("alert"));avigate to http://practice.cybertekschool.com/dynamic_loading
+        driver.get("http://practice.cybertekschool.com/dynamic_loading");
+        // 2. Click on Example 7
+        driver.findElement(By.partialLinkText("Example 7")).click();
+
+        WebElement alertArea = driver.findElement(By.id("alert"));
+        System.out.println("alertArea.getText() = " + alertArea.getText());
+
+        WebDriverWait wait = new WebDriverWait(driver, 6);
+        // Wait for the image show up
+        wait.until(visibilityOfElementLocated(By.xpath("//img[@alt='square pants']")));
+        // Here we had to relocate the element because the attribute has changed (color and stuff),
+        // so driver cannot find it anymore
+        // Element is not in the dom anymore error
+        alertArea = driver.findElement(By.id("alert"));
+        System.out.println("alertArea.getText() = " + alertArea.getText());
     }
 }
