f47f85852bbd5e1c6ed2e0bd970a0f208d24ede2
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-25 16:37:39 -0500

    Day 10: loginErrorMsgVisible method to check the error message

--- src/test/java/com/cybertek/utility/WebOrderUtil.java
+++ src/test/java/com/cybertek/utility/WebOrderUtil.java
@@ -1,7 +1,6 @@
 package com.cybertek.utility;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 public class WebOrderUtil {
 
@@ -26,5 +25,34 @@ public class WebOrderUtil {
         Driver.getDriver().findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
         // Click login
         Driver.getDriver().findElement(By.id("ctl00_MainContent_login_button")).click();
+    }
+
+    /**
+     * A method for logging into Web Order practice site from login page
+     *
+     * @param username username for webOrder app
+     * @param password password for webOrder app
+     */
+
+    public static void login(String username, String password) {
+
+        // Enter username
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_username")).sendKeys(username);
+        // Enter password
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_password")).sendKeys(password);
+        // Click login
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_login_button")).click();
+    }
+
+    /**
+     * Check for login error message is visible or not, by calling the BrowserUtil method we created
+     * @return true if error message displayed, false if not
+     */
+
+    public static boolean loginErrorMsgVisible() {
+
+        boolean elementFound =
+                BrowserUtil.checkVisibilityOfElement(By.xpath("//span[. ='Invalid Login or Password.']"), 2);
+        return elementFound;
     }
 }
