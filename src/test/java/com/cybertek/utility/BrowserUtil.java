5b0424573db4719c95dbc466a8ce99bee1a11721
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-25 16:19:13 -0500

    Day 10: Adding utility method for checking for visibility of an element within timeframe

--- src/test/java/com/cybertek/utility/BrowserUtil.java
+++ src/test/java/com/cybertek/utility/BrowserUtil.java
@@ -1,5 +1,10 @@
 package com.cybertek.utility;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BrowserUtil {
 
     /*
@@ -15,5 +20,39 @@ public class BrowserUtil {
             e.printStackTrace();
         }
 
+    }
+
+    /*
+     *         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 2);
+     *         // Check of visibility of the errorMsgElm in 2 seconds
+     *         try {
+     *             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[. ='invalid Login or Password.']")));
+     *         } catch (TimeoutException e) {
+     *             // System.out.println("The error message is = " + e.getMessage());
+     *             System.out.println("We did not see the error message element");
+     *         }
+     */
+
+    /**
+     * This method will check for visibility of element within the time given
+     *
+     * @param locator By.id or By.xpath or By.whatever
+     * @param timeToWait  time to wait
+     * @return true if the element is found within the time and visible, false if not
+     */
+    public static boolean checkVisibilityOfElement(By locator, int timeToWait) {
+
+        boolean result = false;
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 2);
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            // If above line does not throw exception we will come to this line and below
+            result = true;
+        } catch (TimeoutException e) {
+            // System.out.println("The error message is = " + e.getMessage());
+            System.out.println("We did not see the error message element");
+        }
+        return result;
     }
 }
