ab80395b25e820c5e9d8c2d68bc3fa718c18f561
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-26 18:22:58 -0500

    Day 11: Calling Error Message method

--- src/test/java/com/cybertek/tests/day11_properties_driver_faker_pom/WebOrderPOM_Test.java
+++ src/test/java/com/cybertek/tests/day11_properties_driver_faker_pom/WebOrderPOM_Test.java
@@ -5,6 +5,8 @@ import com.cybertek.utility.BrowserUtil;
 import com.cybertek.utility.TestBase;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class WebOrderPOM_Test extends TestBase {
 
     @Test
@@ -17,6 +19,7 @@ public class WebOrderPOM_Test extends TestBase {
 //        loginPage.passwordField.sendKeys("test");
 //        loginPage.loginButton.click();
         loginPage.login("Tester", "test");
+        assertTrue(loginPage.loginErrorMsgPresent());
 
         BrowserUtil.waitFor(4);
     }
