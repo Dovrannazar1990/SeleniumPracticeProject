f9e2ca8c8391ad26731b5263ab7e42a055ae637c
Dovrannazar1990 <adovrannazar@yahoo.com>
2021-10-26 18:01:14 -0500

    Day 11: Calling goTo method and login method in test

---
+++ src/test/java/com/cybertek/tests/day11_properties_driver_faker_pom/WebOrderPOM_Test.java
@@ -0,0 +1,23 @@
+package com.cybertek.tests.day11_properties_driver_faker_pom;
+
+import com.cybertek.pages.WLoginPage;
+import com.cybertek.utility.BrowserUtil;
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class WebOrderPOM_Test extends TestBase {
+
+    @Test
+    public void testWithPOM_for_login() {
+
+        WLoginPage loginPage = new WLoginPage();
+
+        loginPage.goTo();
+//        loginPage.userNameField.sendKeys("Tester");
+//        loginPage.passwordField.sendKeys("test");
+//        loginPage.loginButton.click();
+        loginPage.login("Tester", "test");
+
+        BrowserUtil.waitFor(4);
+    }
+}
